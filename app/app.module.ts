78270a8b9ae03f88e257b66cb9928588e48c854a
ThunderSpoon <wontae.lee09@gmail.com>
2017-01-13 23:24:57 +1000

    updated search function

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-
+import { ProductFilterPipe } from './products/product-filter.pipe';
 @NgModule({
   imports: [ BrowserModule,
   FormsModule ],
-  declarations: [ 
+  declarations: [
     AppComponent,
-    ProductListComponent],
+    ProductListComponent,
+    ProductFilterPipe],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
