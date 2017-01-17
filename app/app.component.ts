37062f6a5808fcaf9c1493ebff76eaaffd76118f
ThunderSpoon <wontae.lee09@gmail.com>
2017-01-17 17:50:22 +1000

    updated inject

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from './products/product.service';
 
 @Component({
     selector:'pm-app',
     template:`<h1>{{pageTile}}</h1>
             <pm-products></pm-products>
-    `
+    `,
+    providers:[ProductService]
 })
 
 export class AppComponent{
